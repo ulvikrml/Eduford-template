6176c2cf21367a635a3e74abee7c12b4966d1b07
ulvikrml <ulvikrml@gmail.com>
2022-06-28 21:23:50 +0400

    added preloader

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ let openBtn = document.querySelector(".fa-bars");
 let closeBtn = document.querySelector(".fa-times");
 let menu = document.querySelector(".menu");
 let body = document.querySelector("body");
+let loader = document.querySelector('.preloader');
+
+window.addEventListener('load',()=>{
+    setTimeout(function() {
+        loader.style.display='none';
+      }, 1000);
+})
 openBtn.addEventListener("click", ()=>{
     menu.style.right = "0px";
 });
